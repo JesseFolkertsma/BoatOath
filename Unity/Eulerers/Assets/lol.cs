8553a9de62a37d1a902c2c3440238cab8ca47f78
JesseFolkertsma <jesse.folkertsma@gmail.com>
2017-04-02 12:14:27 +0200

    wat

---
+++ Unity/Eulerers/Assets/lol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lol : MonoBehaviour {
+
+    public int intBooiiii = 0;
+    
+	void Start ()
+    {
+        Calculate();
+	}
+
+    void Calculate()
+    {
+        intBooiiii = 0;
+        for (int i = 0; i < 1000; i++)
+        {
+            if (IsInt(i / 3f) || IsInt(i / 5f))
+            {
+                intBooiiii += i;
+            }
+        }
+    }
+
+    bool IsInt(float f)
+    {
+        return Mathf.Floor(f) == f;
+    }
+}
